c60a68e4926aaffb93c166b1c212195508ad6aca
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-15 23:37:55 -0600

    Create punto 2 codigo.py
    Preguntar al usuario nombre, edad, dirección y teléfono y lo guarde en un diccionario. 
    Después debe mostrar por pantalla el mensaje 
    <nombre> 
    tiene <edad> años, 
    vive en <dirección> 
    y su número de teléfono es <teléfono>

---
+++ punto 2 codigo.py
@@ -0,0 +1,12 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("---------------PUNTO 2---------------")
+#capturar valores
+name = input("Cual es tu nombre:")
+edad = input("Cuantos años tienes:")
+direccion = input("Cual es tu direccion:")
+telefono = input("Cual es tu telefono:")
+#despues de ingresar los balores puedes continuar y ver el mensaje
+print(f"<{name}> \nTienes <{edad}> años \nvive en <{direccion}> \ny su número de teléfono es <t{telefono}>")
+print("-------------------------------------")#este programa es un refito
+print("Objetivo: Preguntar al usuario nombre, edad, dirección y teléfono y lo guarde en un diccionario. Después debe mostrar por pantalla el mensaje <nombre> tiene <edad> años, vive en <dirección> y su número de teléfono es <teléfono>")
+print("Resultado: el programa funsiona y despliega el mensaje bien sin errores\n")
